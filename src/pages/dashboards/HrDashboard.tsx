92b34b4e3393ee584273045140a49c399f01aed2
shivamgiri-sudo <shivam.giri@teammas.in>
2026-07-15 15:27:42 +0530

    style: apply approved dashboard reference skin to HR view

--- src/pages/dashboards/HrDashboard.tsx
+++ src/pages/dashboards/HrDashboard.tsx
@@ -1,5 +1,6 @@
 import RoleDashboardV3 from "./RoleDashboardV3";
+import "./role-dashboard-reference.css";
 
 export default function HrDashboard() {
-  return <RoleDashboardV3 variant="hr" />;
+  return <div className="role-dashboard-reference"><RoleDashboardV3 variant="hr" /></div>;
 }
