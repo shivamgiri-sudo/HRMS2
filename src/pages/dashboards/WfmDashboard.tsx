e5f348027b437f78bafd4602a4a91d8a4b325259
shivamgiri-sudo <shivam.giri@teammas.in>
2026-07-15 15:27:29 +0530

    style: apply approved dashboard reference skin to WFM view

--- src/pages/dashboards/WfmDashboard.tsx
+++ src/pages/dashboards/WfmDashboard.tsx
@@ -1,5 +1,6 @@
 import RoleDashboardV3 from "./RoleDashboardV3";
+import "./role-dashboard-reference.css";
 
 export default function WfmDashboard() {
-  return <RoleDashboardV3 variant="wfm" />;
+  return <div className="role-dashboard-reference"><RoleDashboardV3 variant="wfm" /></div>;
 }
