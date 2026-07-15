0f50c1bea6c8933480b4390efb92f01f2eab75ed
shivamgiri-sudo <shivam.giri@teammas.in>
2026-07-15 15:28:22 +0530

    style: apply approved dashboard reference skin to employee view

--- src/pages/dashboards/EmployeeSelfDashboard.tsx
+++ src/pages/dashboards/EmployeeSelfDashboard.tsx
@@ -1,5 +1,6 @@
 import RoleDashboardV3 from "./RoleDashboardV3";
+import "./role-dashboard-reference.css";
 
 export default function EmployeeSelfDashboard() {
-  return <RoleDashboardV3 variant="employee" />;
+  return <div className="role-dashboard-reference"><RoleDashboardV3 variant="employee" /></div>;
 }
