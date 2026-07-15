dca3e199095957202cb52af8e35a6e9b25a66f69
shivamgiri-sudo <shivam.giri@teammas.in>
2026-07-15 15:28:12 +0530

    style: apply approved dashboard reference skin to manager view

--- src/pages/dashboards/ManagerDashboard.tsx
+++ src/pages/dashboards/ManagerDashboard.tsx
@@ -1,5 +1,6 @@
 import RoleDashboardV3 from "./RoleDashboardV3";
+import "./role-dashboard-reference.css";
 
 export default function ManagerDashboard() {
-  return <RoleDashboardV3 variant="manager" />;
+  return <div className="role-dashboard-reference"><RoleDashboardV3 variant="manager" /></div>;
 }
