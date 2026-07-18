ccbff9bc52c6d3707c00c29d57fd87167ccac40b
shivamgiri-sudo <shivam.giri@teammas.in>
2026-07-18 21:13:30 +0530

    feat: include GRN allocation P&L attribution in DBA bootstrap

--- backend/sql/000_finance_supplemental.sql
+++ backend/sql/000_finance_supplemental.sql
@@ -10,3 +10,4 @@ SOURCE sql/414_finance_grn_sequence.sql;
 SOURCE sql/415_bpo_pnl_revenue_cost_model.sql;
 SOURCE sql/416_smart_grn_allocation_document_intelligence.sql;
 SOURCE sql/417_budget_subhead_coverage_control.sql;
+SOURCE sql/418_grn_allocation_pnl_attribution.sql;
